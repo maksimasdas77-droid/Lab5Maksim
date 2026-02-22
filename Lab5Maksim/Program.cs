7c183111039bbe1156381393e3da4de988e8daf2
Максим Дмитрачёнок <maksimasdas77@gmail.com>
2026-02-22 22:37:12 +0300

    Добавьте файлы проекта.

---
+++ Lab5Maksim/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5Maksim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while (run)
+            {
+                menu.Show();
+                int choice = readclas.ReadValue<int>("Выберите пункт меню: ", int.TryParse);
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Выход из программы...");
+                        run = false;
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    default:
+                        Console.WriteLine("Нет такого пункта меню");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+
+        }
+    }
+}
